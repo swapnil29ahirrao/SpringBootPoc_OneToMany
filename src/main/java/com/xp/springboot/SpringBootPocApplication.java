0a52cf79b55d8ee855cb2d12378e7fb1a9be5045
sahirrao <sahirrao@XIPL9639.xpanxion.co.in>
2020-03-19 22:41:45 +0530

    Refactored code

--- src/main/java/com/xp/springboot/SpringBootPocApplication.java
+++ src/main/java/com/xp/springboot/SpringBootPocApplication.java
@@ -1,18 +1,7 @@
 package com.xp.springboot;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import com.xp.springboot.entities.Address;
-import com.xp.springboot.entities.Employee;
-import com.xp.springboot.repository.EmployeeRepository;
-import com.xp.springboot.services.EmployeeService;
 
 @SpringBootApplication
 public class SpringBootPocApplication{
--- src/main/java/com/xp/springboot/controller/EmployeeController.java
+++ src/main/java/com/xp/springboot/controller/EmployeeController.java
@@ -2,8 +2,6 @@ package com.xp.springboot.controller;
 
 import java.util.List;
 
-import javax.websocket.server.PathParam;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,7 +14,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.xp.springboot.entities.Address;
 import com.xp.springboot.entities.Employee;
 import com.xp.springboot.services.EmployeeService;
 
@@ -29,20 +26,20 @@ public class EmployeeController {
 	
 	@GetMapping
 	public ResponseEntity<List<Employee>> getAllEmployee(){
-		return new ResponseEntity<List<Employee>>(empService.getAllEmployee(),HttpStatus.OK);
+		return new ResponseEntity<>(empService.getAllEmployee(),HttpStatus.OK);
 	}
 	
 	@GetMapping("/{emp_id}")
-	public ResponseEntity<Employee> getEmployee(@PathVariable("emp_id") int emp_id){
-		Employee emp=empService.getEmployee(emp_id);
-		return new ResponseEntity<Employee>(emp,HttpStatus.OK);
+	public ResponseEntity<Employee> getEmployee(@PathVariable("emp_id") int empId){
+		Employee emp=empService.getEmployee(empId);
+		return new ResponseEntity<>(emp,HttpStatus.OK);
 	}
 	
 	@PostMapping
 	@PreAuthorize("hasRole('ROLE_ADMIN')")
 	public ResponseEntity<Employee> createOrUpdateEmployee(@RequestBody Employee employee){
 		Employee emp=empService.createOrUpdateEmployee(employee);
-		return new ResponseEntity<Employee>(emp,HttpStatus.OK);
+		return new ResponseEntity<>(emp,HttpStatus.OK);
 	}
 	
 	@DeleteMapping("/{emp_id}")
